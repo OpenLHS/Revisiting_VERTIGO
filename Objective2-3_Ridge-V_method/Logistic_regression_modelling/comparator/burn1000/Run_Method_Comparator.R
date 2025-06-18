c02266e909047578c98b2c5b44a1c6b6b637a89b
Jean-Philippe Morissette <jeanp34@hotmail.com>
2025-06-18 09:24:01 -0400

    Moved comparator to a higher level

--- Objective2-3_Ridge-V_method/Logistic_regression_modelling/comparator/burn1000/Run_Method_Comparator.R
+++ Objective2-3_Ridge-V_method/Logistic_regression_modelling/comparator/burn1000/Run_Method_Comparator.R
@@ -8,37 +8,37 @@ library(this.path)
 
 # Run VERTIGO-CI 
 setwd(this.dir())
-setwd("../../../../../Objective1_ConfidentialityIssues/Objective1a_VERTIGO-CI/")
+setwd("../../../../Objective1_ConfidentialityIssues/Objective1a_VERTIGO-CI/")
 source("Run_VERTIGO-CI.R")
 
 # Run RIDGE_V
 setwd(this.dir())
-setwd("../distributed/")
+setwd("../../examples/burn1000_same_folder/distributed/")
 source("Run_Example.R")
 
 # Run glmnet
 setwd(this.dir())
-setwd("../pooled/")
+setwd("../../examples/burn1000_same_folder/pooled/")
 source("Run_Pooled_Example.R")
 
 # Load VERTIGO-CI output
 setwd(this.dir())
-setwd("../../../../../Objective1_ConfidentialityIssues/Objective1a_VERTIGO-CI/")
+setwd("../../../../Objective1_ConfidentialityIssues/Objective1a_VERTIGO-CI/")
 VERTIGO_CI_output <- read.csv("VERTIGO_CI_output.csv")
 VERTIGO_CI_output <- VERTIGO_CI_output[,-3]
 
 # Load and combine RIDGE-V outputs
 setwd(this.dir())
-setwd("../distributed/")
+setwd("../../examples/burn1000_same_folder/distributed/")
 RidgeV_CC_output <- read.csv("Coord_node_results_distributed_log_regV.csv")
 RidgeV_Node1_output <- read.csv("Data_node_1_results.csv")
 RidgeV_Node2_output <- read.csv("Data_node_2_results.csv")
 RidgeV_Node3_output <- read.csv("Data_node_3_results.csv")
 RidgeV_combined_output <- rbind(c("intercept", RidgeV_CC_output[1,2]), RidgeV_Node1_output, RidgeV_Node2_output, RidgeV_Node3_output) # attention type de données ici
 
-# Load pooled output
+# Load glmnet pooled output
 setwd(this.dir())
-setwd("../pooled/")
+setwd("../../examples/burn1000_same_folder/pooled/")
 Pooled_output <- read.csv("Pooled_Results_log_regV.csv")
 
 # Create a single df to compare results
